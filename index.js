c64ee2360fc47572b3be624cfd6fa6576070bb41
yerinko <yerin_o3o@naver.com>
2020-12-15 00:26:33 +0900

    토큰 생성 with jsonwebtoken

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express'); // package.json 에 있는 express 모듈을
 const app = express(); // express을 이용하여 app을 만든다.
 const port = 3000;
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./model/User");
 
 // application/x-www-form-urlencoded  와 json 형태를 가져와
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({extend:true}));
 app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://yerinko:kate0523@@@boilerplate.b4kwc.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected…"))
     .catch(err => console.log(err));
@@ -33,6 +36,36 @@ app.post('/register', (req, res) => {
         })
     })
 
+});
+
+app.post('/login', (req, res) => {
+
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, userInfo => { // findOne()은 mongoDB에서 제공하는 메소드이다.
+        if(!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다. "
+            })
+        }
+        // 요청한 이메일이 DB에 있다면? 비밀번호가 맞는 비밀번호인지 먼저 확인.
+        user.comparePassword(req.body.password , (err, isMatch) => {
+            if(!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."
+
+                // 비밀번호까지 맞다면 토큰을 생성하기.
+                    user.generateToken((err, user) => {
+
+                    })
+        })
+
+
+        })
+    }))
+
+
+
+    // 비밀번호까지 같다면 Token 생성하기.
 });
 
 app.listen(port, () => {
